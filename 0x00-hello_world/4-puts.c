b7ad178aa98f8a43db49bc9a05b564d0fbacb136
Emogy395 <emogy008@gmail.com>
2022-09-09 12:13:14 -0700

    Low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Prints "programming is like building a multilingual
+ *puzzle, followed ba a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
