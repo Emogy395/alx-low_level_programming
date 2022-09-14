dbaa181dcf79c55c19b96a2c136ea56698781810
Emogy395 <emogy008@gmail.com>
2022-09-13 22:40:50 -0700

    My Print _putchur

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - a function prototype
+ * File: main.h
+ * Desc: This is an header file created to contain
+ * all function declarations used in the
+ * 0x02-functions_nested_loops directory.
+ *
+ * @c: an argument
+ * Return: No return value here, it's just a function prototype
+*/
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
