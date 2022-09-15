bcfeb379ddd184f4d5b51324a86408cbc5ea82c2
Emogy395 <emogy008@gmail.com>
2022-09-15 03:02:05 -0700

    My function nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,7 +21,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_teble(void);
+void time_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_to_98(int n);
