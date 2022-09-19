8ca63e7be948c5c31c8bcf8d7503b22bcce088d7
Emogy395 <emogy008@gmail.com>
2022-09-19 21:01:12 +0000

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the lenth of a string.
+ *
+ * @: the string to calculate it's length.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int lengtth = 0
+
+		for (i =0; s[i] != '\0'; i++)
+		{
+		       length++;
+		}
+	return (length);
+}
